11acd59b684e053d21ec145910969997a01737fc
о <>
2017-10-17 11:41:01 +0300

    Добавил проект SimpleMetadata

---
+++ SimpleMetadata/SimpleMetadata/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Management;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        class Range
+        {
+            public float From { get; set; }
+            public float To { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var v1 = new Vector(1, 2);
+            var v2 = new Vector(8, 6);
+
+            var r = new Range { From = 5, To = 3 };
+
+            Console.WriteLine(VectorToString(v1));
+            Console.WriteLine(VectorToString(v2));
+            Console.WriteLine(VectorToString(r));
+
+            var v3 = VectorToString<Vector>("Y=98;");
+            Console.WriteLine(v3);
+
+            Console.ReadLine();
+        }
+
+        public static T VectorToString<T>(string data)
+        {
+            var type = typeof(T);
+
+            var props_tuple = data.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var obj = Activator.CreateInstance<T>();
+
+            foreach (var tuple in props_tuple)
+            {
+                var t = tuple.Split('=');
+                var prop = type.GetProperty(t[0]);
+
+                prop.SetValue(obj, Convert.ChangeType(t[1], prop.PropertyType));
+            }
+
+            return obj;
+        }
+
+        public static string VectorToString<T>(T obj)
+        {
+            var result = "";
+
+            foreach (var prop in obj.GetType().GetProperties())
+            {
+                var val = prop.GetValue(obj);
+                result += string.Format("{0}={1};", prop.Name, val);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SimpleMetadata/SimpleMetadata/Vector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class Vector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector()
+        {
+        }
+
+        public Vector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        private void Add(float V)
+        {
+            X += V;
+            Y += V;
+        }
+
+        public bool IsZero()
+        {
+            return X == 0 && Y == 0;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static Vector operator *(Vector a, Vector b)
+        {
+            return new Vector(a.X * b.X, a.Y * b.Y);
+        }
+
+        public static Vector operator /(Vector a, Vector b)
+        {
+            return new Vector(a.X / b.X, a.Y / b.Y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X: {0}, Y: {1}", X, Y);
+            //return $"X: {X}, Y: {Y}";     // in C#7.0
+        }
+    }
+}
