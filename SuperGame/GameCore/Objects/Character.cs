ddaeff2495f6209a94c03692e97036c4d96a328c
Михаил Кривошея <mrkriv@bk.ru>
2017-11-21 13:54:52 +0300

    Разделил Player и Character

---
+++ SuperGame/GameCore/Objects/Character.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using GameCore.Render;
+
+namespace GameCore.Objects
+{
+    public class Character : MapObject
+    {
+        public Dictionary<string, Animation> Animations;
+
+        public float Speed { get; set; }
+        public Vector2 Velocity { get; set; }
+
+        public Character()
+        {
+            Animations = new Dictionary<string, Animation>
+            {
+                {
+                    "idle", new Animation
+                    {
+                        PathMask = "player\\idle_{0}",
+                        ImageCount = 4,
+                        Speed = 3
+                    }
+                },
+                {
+                    "walk", new Animation
+                    {
+                        PathMask = "player\\walk_{0}",
+                        ImageCount = 4,
+                        Speed = .2f
+                    }
+                },
+                {
+                    "run", new Animation
+                    {
+                        PathMask = "player\\run_{0}",
+                        ImageCount = 4,
+                        Speed = .2f
+                    }
+                }
+            };
+
+            Speed = 250;
+        }
+
+        public override void OnTick(float dt)
+        {
+            var posDelta = Velocity * dt;
+
+            Position += posDelta;
+
+            if (posDelta == Vector2.Zero)
+            {
+                Animations["idle"].OnTick(dt, RenderPrimitive);
+            }
+            else
+            {
+                if (Velocity.LengthSquared() > Speed * Speed)
+                {
+                    Animations["run"].OnTick(dt, RenderPrimitive);
+                }
+                else
+                {
+                    Animations["walk"].OnTick(dt, RenderPrimitive);
+                }
+            }
+
+            base.OnTick(dt);
+        }
+    }
+}
--- SuperGame/GameCore/Objects/Player.cs
+++ SuperGame/GameCore/Objects/Player.cs
@@ -6,50 +6,13 @@ using GameCore.Render;
 
 namespace GameCore.Objects
 {
-    public class Player : MapObject
+    public class Player : Character
     {
-        public Dictionary<string, Animation> Animation;
-
-        public float Speed { get; set; }
-
-        public Player()
-        {
-            Animation = new Dictionary<string, Animation>
-            {
-                {
-                    "idle", new Animation
-                    {
-                        PathMask = "player\\idle_{0}",
-                        ImageCount = 4,
-                        Speed = 3
-                    }
-                },
-                {
-                    "walk", new Animation
-                    {
-                        PathMask = "player\\walk_{0}",
-                        ImageCount = 4,
-                        Speed = .2f
-                    }
-                },
-                {
-                    "run", new Animation
-                    {
-                        PathMask = "player\\run_{0}",
-                        ImageCount = 4,
-                        Speed = .2f
-                    }
-                }
-            };
-
-            Speed = 250;
-        }
-
         public override void OnTick(float dt)
         {
             var dir = new Vector2();
             var speed = Speed;
-
+            
             if (World.InputManager.IsKeyDown(Key.W))
                 dir += new Vector2(0, -1);
 
@@ -61,37 +24,19 @@ namespace GameCore.Objects
 
             if (World.InputManager.IsKeyDown(Key.D))
                 dir += new Vector2(1, 0);
-            
+
             if (World.InputManager.IsKeyDown(Key.LeftShift))
                 speed *= 1.5f;
-
+            
             if (dir != Vector2.Zero)
                 dir = dir / dir.Length();
 
-            var posDelta = dir * speed * dt;
+            Velocity = dir * speed;
 
-            Position += posDelta;
-
-            if (posDelta == Vector2.Zero)
-            {
-                Animation["idle"].OnTick(dt, RenderPrimitive);
-            }
-            else
-            {
-                if (World.InputManager.IsKeyDown(Key.LeftShift))
-                {
-                    Animation["run"].OnTick(dt, RenderPrimitive);
-                }
-                else
-                {
-                    Animation["walk"].OnTick(dt, RenderPrimitive);
-                }
-            }
+            base.OnTick(dt);
 
             if (World.RenderManager != null)
                 World.RenderManager.ActiveCamera.Position = new Vector3(Position, 0);
-            
-            base.OnTick(dt);
         }
     }
 }
